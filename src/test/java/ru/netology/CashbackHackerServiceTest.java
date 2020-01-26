f18d2993c3e9ea0202a7102298cc675007e1f4f5
Mihail Galkov <gaus.spb@gmail.com>
2020-01-26 12:42:12 +0300

    Service version 2 with CI

--- src/test/java/ru/netology/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/CashbackHackerServiceTest.java
@@ -24,22 +24,4 @@ class CashbackHackerServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldReturn0IfAmountIs1100() {
-        CashbackHackerService cashbackHacker = new CashbackHackerService();
-        int amount = 1100;
-        int actual = cashbackHacker.remain(amount);
-        int expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldReturn1000IfAmountIs0() {
-        CashbackHackerService cashbackHacker = new CashbackHackerService();
-        int amount = 0;
-        int actual = cashbackHacker.remain(amount);
-        int expected = 1000;
-        assertEquals(expected, actual);
-    }
-
 }
